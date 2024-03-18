c74d2cfde7c013b36e154d142f4285ea8084f366
Mesfin Desale <147912364+messfind@users.noreply.github.com>
2024-03-17 23:20:25 -0700

    Update NavBar.tsx

--- beta/app/NavBar.tsx
+++ beta/app/NavBar.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Link from "next/link"
+import { FaWolfPackBattalion } from "react-icons/fa";
 const NavBar = () => {
   return (
-    <nav>
-          <Link href="/">Logo</Link>
-          <ul>
+    <nav className='flex space-x-5 border-b px-5'>
+
+          <Link href="/"><FaWolfPackBattalion/></Link>
+          <ul className='flex space-x-4 hover:text-red-950'>
               <li>
                   <Link href="/">Dashboard</Link>
               </li>
