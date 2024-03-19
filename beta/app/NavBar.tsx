26760abed65ba5386f5ec672d7edbb8f3faf11a7
Mesfin Desale <147912364+messfind@users.noreply.github.com>
2024-03-19 01:28:36 -0700

    Update NavBar.tsx

--- beta/app/NavBar.tsx
+++ beta/app/NavBar.tsx
@@ -5,20 +5,10 @@ import Links from '@/components/navbar/lonks/Links';
 Links
 const NavBar = () => {
   return (
-    <nav className='flex space-x-5 border-b px-5'>
+    <nav className='bg-[#0400ff]'>
 
       <Links/>
           <Link href="/"><FaWolfPackBattalion/></Link>
-          {/* <ul className='flex space-x-4 hover:text-red-950'> */}
-              {/* <li> */}
-                  {/* <Link href="/">Dashboard</Link> */}
-              {/* </li> */}
-              {/* <li> */}
-                  {/* <Link href="/issue">Issue</Link> */}
-              {/* </li> */}
-      {/* </ul> */}
-      {/* <Link href="/about">about</Link> */}
-      {/* <Link href="/about/career">about career</Link> */}
 
     </nav>
   )
