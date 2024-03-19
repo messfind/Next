7af24683ceb60a6a9d84065028f9d37a0edd2026
Mesfin Desale <147912364+messfind@users.noreply.github.com>
2024-03-19 00:06:29 -0700

    Create not-found.tsx
    if unexpected url run this one

---
+++ beta/app/not-found.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+const NotFound = () => {
+  return (
+      <div>
+          <h2>page not found</h2>
+      <Link href="/"> Return Home</Link>
+    </div>
+  )
+}
+
+export default NotFound
