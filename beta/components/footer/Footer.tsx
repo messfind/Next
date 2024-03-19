798f5229994e74c5995216b682550f73c6efadf5
Mesfin Desale <147912364+messfind@users.noreply.github.com>
2024-03-19 00:05:28 -0700

    Create Footer.tsx

---
+++ beta/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <h1 className='text-red-600 hover:text-black'>My Footer</h1>
+    </div>
+  )
+}
+
+export default Footer
