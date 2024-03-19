ff3e5e350d8d3695586c661cbd97cf25d1ee6061
Mesfin Desale <147912364+messfind@users.noreply.github.com>
2024-03-19 00:05:36 -0700

    Update page.tsx

--- beta/app/page.tsx
+++ beta/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home(){
   return (
     <div>
-      <h1 className="border-t mt-5 px-5 hover:forced-colors:">Hello world</h1>
+      <h1 className="border-t mt-5 px-5 hover:">Hello world</h1>
     </div>
   )
 }
